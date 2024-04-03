8be647904020f155c0a3afd9c0193ba2befd3a2e
Mixalhs <manblu@hotmail.gr>
2024-04-03 23:38:51 +0300

    mylittlelemon

---
+++ src/App.js
@@ -0,0 +1,32 @@
+//import { useState, useEffect } from "react";
+import "./App.css"
+import React from "react";
+
+function App() { 
+  const [user, setUser] = React.useState([]);
+
+  const fetchData = () => {
+   fetch("https://randomuser.me/api/?results=1")
+    .then(response => response.json())
+    .then(data => setUser(data));
+  }
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+
+  return Object.keys(user).length > 0 ? (
+    <div>
+      <h1>Data returned</h1>
+      <h2>First Name: {user.results[0].name.first}</h2>
+      <h2>Last Name: {user.results[0].name.last}</h2>
+      <h2>Title: {user.results[0].name.title}</h2>
+      <img src={user.results[0].picture.large} alt=""/>
+    </div>
+  ) : (
+    <h1>Data pending...</h1>
+  )
+
+}
+export default App;
